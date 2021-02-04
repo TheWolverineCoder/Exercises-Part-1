056758081456139f88d2c00279d17b1fc4db5369
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-04 17:14:33 +0200

    Add files via upload

---
+++ BitAtN-thPosition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BitAtN_thPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the position of the bit you want: ");
+            int position = int.Parse(Console.ReadLine());
+            int changedNumber = number >> position;
+            int bit = 0;
+            if((changedNumber & 1) == 1)
+            {
+                bit = 1;
+            }
+            else
+            {
+                bit = 0;
+            }
+            Console.WriteLine("The bit at position {0} is {1}.",position,bit);
+        }
+    }
+}
