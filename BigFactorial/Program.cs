389b88da8e9852a58db85b3ba61f7e7de841199b
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-04 17:09:38 +0200

    Add files via upload

---
+++ BigFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number here(1-1000): ");
+            int number = int.Parse(Console.ReadLine());
+
+            BigInteger bigInteger = 1;
+            for(int i = number; i > 0; i--)
+            {
+                bigInteger *= i;
+            }
+
+            Console.WriteLine("The factorial is: ");
+            Console.WriteLine(bigInteger);
+        }
+    }
+}
