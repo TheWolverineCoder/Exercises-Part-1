08fe895e1b979385a3c6d4162bfae1310b006a54
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-18 18:36:22 +0200

    Add files via upload

---
+++ HowToWait/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace EHowToWait
+{
+    class Program
+    {
+        static volatile bool isRunning;
+        static void Worker()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(200);
+            }
+            isRunning = false;
+            Console.WriteLine("Thread is done.");
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Worker);
+
+            isRunning = true;
+            t.Start();
+
+            int i = 0;
+            while (isRunning)
+            {
+                i++;
+            }
+            Console.WriteLine(i);
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Messaging/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The digits of your numbers are summed and stored in a list of indexes
+            //Then after you enter your text
+            //The characters at the given indexes we got are printed as a message
+            Console.WriteLine("Enter your list of numbers: ");
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Console.WriteLine("Enter your text: ");
+            string text = Console.ReadLine();
+            
+            List<char> characters = new List<char>();
+            foreach(char character in text)
+            {
+                characters.Add(character);
+            }
+            List<int> indexes = new List<int>();
+            foreach(int number in numbers)
+            {
+                int currentNumber = number;
+                int sum = 0;
+                while (currentNumber > 0)
+                {
+                    sum += (currentNumber % 10);
+                    currentNumber /= 10;
+                }
+                indexes.Add(sum);
+            }
+
+            string hiddenMessage = "";
+            foreach(int index in indexes)
+            {
+                int selectedChar = index;
+                if(characters.Count <= index)
+                {
+                    selectedChar = index % characters.Count;
+                }
+                hiddenMessage = hiddenMessage + characters[selectedChar];
+                characters.RemoveAt(selectedChar);
+            }
+
+            Console.WriteLine(hiddenMessage);
+        }
+    }
+}
