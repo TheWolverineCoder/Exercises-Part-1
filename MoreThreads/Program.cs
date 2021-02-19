e69d851d763aad197946e11d5b63005948f9aca6
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-19 22:13:22 +0200

    Add files via upload

---
+++ MoreThreads/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace MoreThreads
+{
+    class Program
+    {
+        static bool isRunning = false;
+        static string ThreadResult;
+        static void ThreadWorker(object data)
+        {
+            for (int i = 0; i < (int)data; i++)
+            {
+                Thread.Sleep(2000);
+            }
+            ThreadResult = "42";
+            isRunning = false;
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(start: ThreadWorker);
+            isRunning = true;
+            Console.Write("Progress: ");
+
+            t1.Start(5);
+
+            while (isRunning)
+            {
+                Console.Write(".");
+                Thread.Sleep(300);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Thread result: " + ThreadResult);
+
+            Console.WriteLine("Press ENTER to quit.");
+            Console.ReadLine();
+
+        }
+    }
+}
