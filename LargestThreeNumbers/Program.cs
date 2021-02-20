2856ba917acfcb513e7824f9aa59d5504f94b567
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-20 19:14:48 +0200

    Add files via upload

---
+++ LargestThreeNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LargestThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your list of numbers: ");
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbersSorted = numbers.OrderByDescending(n => n).ToArray();
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write(numbersSorted[i] + " ");
+            }
+        }
+    }
+}
