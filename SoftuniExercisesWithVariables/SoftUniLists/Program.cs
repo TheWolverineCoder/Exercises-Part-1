4f7d0824c11745e3ccd26ab3018dfc09b72cebd6
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-21 12:59:43 +0200

    Add files via upload

---
+++ SoftuniExercisesWithVariables/SoftUniLists/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your list of vagons: ");
+            List<int> vagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+        }
+        
+        static List<int> ReadList(int n)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            return list;
+        }
+       
+        static List<int> ReadList()
+        {
+            List<int> list = new List<int>();
+            var line = Console.ReadLine();
+            list = line.Split().Select(int.Parse).ToList();
+            return list;
+        }
+        
+        static List<string> ReadStringList(int number)
+        {
+            List<string> list = new List<string>(number);
+            list = Console.ReadLine().Split().ToList();
+            return list;
+        }
+
+        static void PrintList(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+        
+    }
+}
---
+++ SoftuniExercisesWithVariables/SoftUniMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+
+namespace SoftuniExercisesWithVariables
+{
+    
+    public class SoftUni
+    {
+        public static void Main()
+        {
+            var arr = new int[] { 1, 2, 3, 4, 5 };
+            Increment(arr);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        static void Increment(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]++;
+            }
+        }
+
+
+    }
+}
