bf0ac1dc9a91bc2ec9fb303e04ae1693b573c0ce
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-22 15:33:26 +0200

    Add files via upload

---
+++ Take_SkipRope/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Take_SkipRope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string here: ");
+            string initialMessage = Console.ReadLine();
+            List<int> integers = new List<int>();
+            List<char> chars = new List<char>();
+            
+            for(int i = 0; i < initialMessage.Length; i++)
+            {
+                if(initialMessage[i] >= 48 && initialMessage[i] <= 57)
+                {
+                    integers.Add((initialMessage[i] - '0'));
+                }
+                else
+                {
+                    chars.Add(initialMessage[i]);
+                }
+            }
+
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+            for(int k = 0; k < integers.Count; k++)
+            {
+                if(k%2 == 0)
+                {
+                    takeList.Add(integers[k]);
+                }
+                else
+                {
+                    skipList.Add(integers[k]);
+                }
+            }
+            string result = "";
+            int iterations = takeList.Count;
+            int currentStart = 0;
+            for (int f = 0; f < iterations; f++)
+            {
+
+
+                int increase = takeList[f] + currentStart;
+                if(increase > chars.Count)
+                {
+                    increase = chars.Count;
+                }
+                for (int h = currentStart; h < increase; h++)
+                {
+                    result += chars[h];
+                }
+                
+                currentStart += takeList[f];
+                currentStart += skipList[f];
+
+            }
+
+            Console.WriteLine(result);
+            
+        }
+    }
+}
