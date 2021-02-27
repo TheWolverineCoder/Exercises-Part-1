d7fa5b7a9685df5961e8fce237f31256c76f34d0
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-02-27 20:22:04 +0200

    Add files via upload

---
+++ AMinerTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Dictionary<string, int> dataBase = new Dictionary<string, int>();
+            while((input = Console.ReadLine()) != "stop")
+            {
+                string material = input;
+                string quant = Console.ReadLine();
+                if(quant == "stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(quant);
+                if (dataBase.ContainsKey(material))
+                {
+                    dataBase[material] += quantity;
+                }
+                else
+                {
+                    dataBase.Add(material, quantity);
+                }
+            }
+
+            foreach (var materi in dataBase)
+            {
+                Console.WriteLine(materi.Key + " -> " + materi.Value);
+            }
+        }
+    }
+}
