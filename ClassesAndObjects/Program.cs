20cdfc96abb793ebacebb80579f689791de52092
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-03 18:19:49 +0200

    Add files via upload

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string[] input = Console.ReadLine().Split(" ");
+            for(int i = 0; i < input.Length; i++)
+            {
+                int index = random.Next(i, input.Length);
+                string help = input[i];
+                input[i] = input[index];
+                input[index] = help;
+            }
+
+            foreach(string word in input)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
