e0e3f728ef07a59a7c11a7b2e3ca4a069bc7229d
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-06 00:03:19 +0200

    Add files via upload

---
+++ OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your line of words(separated by space): ");
+            string[] words = Console.ReadLine().Split(" ");
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
+
+            foreach(string word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (occurrences.ContainsKey(wordInLowerCase))
+                {
+                    occurrences[wordInLowerCase]++;
+                }
+                else
+                {
+                    occurrences.Add(wordInLowerCase, 1);
+                }
+            }
+
+            foreach(var occurrence in occurrences)
+            {
+                if(occurrence.Value % 2 != 0)
+                {
+                    Console.WriteLine(occurrence.Key + " ");
+                }
+            }
+        }
+    }
+}
