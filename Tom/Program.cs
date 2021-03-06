87cdc9dcad1c2e582c1d6a15eb3a41b614e8ac03
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-06 20:43:08 +0200

    Add files via upload

---
+++ Tom/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace Tom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] priceRatings = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int entryPoint = int.Parse(Console.ReadLine());
+            string itemsType = Console.ReadLine();
+            int leftElements = 0;
+            int rightElements = 0;
+            for(int i = 0; i < entryPoint; i++)
+            {
+                if(itemsType == "cheap")
+                {
+                    if(priceRatings[i] < priceRatings[entryPoint])
+                    {
+                        leftElements += priceRatings[i];
+                    }
+                    else
+                    {
+                        leftElements += 0;
+                    }
+                }
+                else
+                {
+                    if (priceRatings[i] >= priceRatings[entryPoint])
+                    {
+                        leftElements += priceRatings[i];
+                    }
+                    else
+                    {
+                        leftElements += 0;
+                    }
+                }
+            }
+
+            for(int i = priceRatings.Length - 1; i > entryPoint; i--)
+            {
+                if (itemsType == "cheap")
+                {
+                    if (priceRatings[i] < priceRatings[entryPoint])
+                    {
+                        rightElements += priceRatings[i];
+                    }
+                    else
+                    {
+                        rightElements += 0;
+                    }
+                }
+                else
+                {
+                    if (priceRatings[i] >= priceRatings[entryPoint])
+                    {
+                        rightElements += priceRatings[i];
+                    }
+                    else
+                    {
+                        rightElements += 0;
+                    }
+                }
+            }
+
+            if(leftElements >= rightElements)
+            {
+                Console.WriteLine("Left - " + leftElements);
+            }
+            else
+            {
+                Console.WriteLine("Right - " + rightElements);
+            }
+        }
+    }
+}
