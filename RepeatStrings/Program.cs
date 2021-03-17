c3ef0871b86982616ad6eaeb9fa67826ee3a8e24
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-17 11:53:32 +0200

    Add files via upload

---
+++ RepeatStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArr = { "Dog", "Cat", "Mouse" };
+            string line = "";
+            foreach(string word in stringArr)
+            {
+                for(int i = 0; i < word.Length; i++)
+                {
+                    line += word;
+                }
+            }
+            Console.WriteLine(line);
+        }
+    }
+}
