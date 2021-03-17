0fb8997ff7164ab4c6faddd613e77743cd104e5e
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-17 20:48:44 +0200

    Add files via upload

---
+++ TextFilter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter banned word separated with \", \": ");
+            string[] banList = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (string word in banList)
+            {
+                int length = word.Length;
+                string newOne = "";
+                for(int i = 0; i < length; i++)
+                {
+                    newOne += "*";
+                }
+                text = text.Replace(word, newOne);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
