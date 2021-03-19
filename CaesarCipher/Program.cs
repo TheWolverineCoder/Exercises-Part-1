d8acb5986f7d2ee1f490e8274c7b93ca6dfb4c40
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-19 16:11:04 +0200

    Add files via upload

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string here: ");
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder("");
+            foreach(char ch in input)
+            {
+                int code = (int)ch + 3;
+                char newOne = (char)code;
+                sb.Append(newOne);
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
