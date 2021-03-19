1d9f943e371b70b9376ce5030609807780cec17f
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-19 16:05:25 +0200

    Add files via upload

---
+++ ExtractFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your directory: ");
+            string directory = Console.ReadLine();
+            string[] separated = directory.Split("\\");
+            string file = separated[separated.Length - 1];
+            string[] fileConc = file.Split(".");
+            Console.WriteLine("File name: " + fileConc[0]);
+            Console.WriteLine("File extension: " + fileConc[1]);
+
+        }
+    }
+}
