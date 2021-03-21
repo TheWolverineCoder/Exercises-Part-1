3e6cb3d616343833185aca20b98c3b123d920dd2
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-21 14:19:46 +0200

    Add files via upload

---
+++ ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string here: ");
+            char[] charArr = Console.ReadLine().ToCharArray();
+            List<char> charResult = new List<char>();
+            charResult.Add(charArr[0]);
+            foreach(char ch in charArr)
+            {
+                if(ch != charResult.Last())
+                {
+                    charResult.Add(ch);
+                }
+            }
+
+            Console.WriteLine(String.Join(String.Empty,charResult));
+        }
+    }
+}
