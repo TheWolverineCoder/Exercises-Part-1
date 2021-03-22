b634993dc4867c24928ae54035a1cd47c3a710f0
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-22 18:40:02 +0200

    Add files via upload

---
+++ ExtractUserInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtractUserInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of lines: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                List<string> words = Console.ReadLine().Split(" ").ToList();
+                string name = "";
+                int age = 0;
+                foreach (string word in words)
+                {
+                    if (word[0] == '@' && (word.Last() == '|'))
+                    {
+                        name = word.Substring(1, word.Length - 2);
+                    }
+
+                    if (word[0] == '#' && (word.Last() == '*'))
+                    {
+                        age = int.Parse(word.Substring(1, word.Length - 2));
+                    }
+
+                }
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+
+        }
+    }
+}
