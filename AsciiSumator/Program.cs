e91de941691ea107bbe8031f6db3b85afbdeb1d0
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-22 19:05:42 +0200

    Add files via upload

---
+++ AsciiSumator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = Console.ReadLine()[0];
+            char secondChar = Console.ReadLine()[0];
+            int sum = 0;
+            string line = Console.ReadLine();
+            foreach(char ch in line)
+            {
+                if(ch >= firstChar && ch <= secondChar)
+                {
+                    sum += (int)ch;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
