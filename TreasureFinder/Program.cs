ab4245492fd438287d6b19a4db40670dfa03d0b8
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-25 22:15:16 +0200

    Add files via upload

---
+++ TreasureFinder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreasureFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter your decryption key: ");
+            int[] key = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            string mes;
+            while ((mes = Console.ReadLine()) != "find")
+            {
+                StringBuilder message = new StringBuilder();
+
+                for (int i = 0; i < mes.Length; i++)
+                {
+                    message.Append(Convert.ToChar(mes[i] - key[i % key.Length]));
+                }
+
+                string messageAsStr = message.ToString();
+
+                int startIndexOfTreasure = messageAsStr.IndexOf("&") + 1;
+                int treasureLength = messageAsStr.LastIndexOf("&") - startIndexOfTreasure;
+
+                string treasure = messageAsStr.Substring(startIndexOfTreasure, treasureLength);
+
+                int startIndexOfCoordinates = messageAsStr.IndexOf("<") + 1;
+                int coordinatesLength = messageAsStr.IndexOf(">") - startIndexOfCoordinates;
+
+                string coordinates = messageAsStr.Substring(startIndexOfCoordinates, coordinatesLength);
+
+                Console.WriteLine($"Found {treasure} at {coordinates}");
+
+                
+
+            }
+
+        }
+    }
+}
